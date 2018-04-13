f6abbe80203287e06ca987548ba9c70e706ff7f9
Wilson Empleo <wilsone@codev.com>
2018-04-14 07:20:03 +0800

    add binary and statement

---
+++ OtherServices/BinaryService/dispute.php
@@ -0,0 +1,35 @@
+<?php
+require_once("../../Authorize.php");
+require_once("../../config.php");
+
+use Authorization\Authorize;
+
+try {
+    $authorize = new Authorize($username, $password, $apiurl.'user/v3/refresh');
+    $access = $authorize->refreshJWT($refreshToken);
+    $JWT = $access->idToken;
+
+    $data = http_build_query([
+        "merchantId" => 2342,
+    ]);
+
+    //SAMPLE  PHP CODE REQUEST STARTS HERE
+    $ch = curl_init($apiurl.'pdf/v3/dispute/'.$data);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Authorization: ".$JWT,
+        "Content-Type: application/json"
+         ));
+    $result = curl_exec($ch);
+
+    curl_close($ch);
+    //SAMPLE  PHP CODE REQUEST ENDS HERE
+
+    echo '<pre>';
+    print_r(json_decode($result));
+    echo '</pre>';
+} catch (Exception $e) {
+    return $e->getMessage();
+}
