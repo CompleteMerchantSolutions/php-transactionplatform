2f8cda3ba284cb3ed6f14231610ee617d50c417d
kenziebrex <kenziebrex@yahoo.com.ph>
2018-04-13 14:37:18 +0800

    checked getMerchantByIds, Count Per Ach Detail, getMatchingTransactionForFDRChargeback and getMatchingTransactionForTsysChargeback

---
+++ TransactionService/count_per_ach_detail.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once("../Authorize.php");
+require_once("../config.php");
+
+use Authorization\Authorize;
+
+try {
+    $authorize = new Authorize($username, $password, $apiurl.'user/v3/refresh');
+    $access = $authorize->refreshJWT($refreshToken);
+
+    $ch = curl_init($apiurl.'transaction/v3/count');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        "Authorization: ".$JWT,
+        "Content-Type: application/json"
+    ));
+    $result = curl_exec($ch);
+
+    curl_close($ch);
+    //SAMPLE  PHP CODE REQUEST ENDS HERE
+
+    echo '<pre>';
+    print_r(json_decode($result));
+    echo '</pre>';
+} catch (Exception $e) {
+    return $e->getMessage();
+}
