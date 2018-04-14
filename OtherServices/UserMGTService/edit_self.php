8b620d1779b27ba973619135c043c5b9e9b376e7
erwinsuico <suicoerw@gmail.com>
2018-04-14 10:47:22 +0800

    tested create_user, edit_self, merchant, user and whoami

---
+++ OtherServices/UserMGTService/edit_self.php
@@ -0,0 +1,37 @@
+<?php
+require_once("../../Authorize.php");
+require_once("../../config.php");
+
+use Authorization\Authorize;
+
+$authorize = new Authorize($username, $password, $apiurl.'user/v3/refresh');
+$access = $authorize->refreshJWT($refreshToken);
+$JWT = $access->idToken;
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $apiurl."user/v3/self",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "PUT",
+  CURLOPT_POSTFIELDS => "{\"firstName\": \"Erwin\", \"lastName\": \"Suico\", \"phone\": \"+639255008103\", \"notifications\": \"test\"}",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: ".$JWT,
+    "Content-Type: application/x-www-form-urlencoded"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
